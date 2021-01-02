4db1153ddcc2dc8f93d6060ca54231fb1e939242
Ubuntu <ubuntu@ip-172-31-23-137.us-east-2.compute.internal>
2021-01-02 08:26:53 +0000

    test

--- machine_learning_for_weather_forecast/web_collect_process_predict.py
+++ machine_learning_for_weather_forecast/web_collect_process_predict.py
@@ -17,16 +17,12 @@ from weather_module import *
 from constant import *
 
 print("start running web_collect_process_predict.py")
-cur_path = os.getcwd()
-print(cur_path)
-
 """collect data"""
 collect_times = 2
 year = 2020
 month = 12
 day = 10
-history_data_file = os.path.join(cur_path, 'prediction_data/historical_data.pkl')
-print(history_data_file)
+history_data_file = 'machine_learning_for_weather_forecast/prediction_data/historical_data.pkl'
 # it will force to collect data from start_date each time. Check carefully each time!
 force_restart = False 
 
@@ -34,7 +30,7 @@ force_restart = False
 collect_data_func(collect_times, year, month, day, history_data_file, force_restart, True)
 
 """load and process"""
-process_data_output =os.path.join(cur_path, 'prediction_data/processed_data.pkl')
+process_data_output = 'machine_learning_for_weather_forecast/prediction_data/processed_data.pkl'
 load_and_process_data_func(history_data_file, process_data_output)
 # remove data of last day for it was collected as dummy data to use date and daytime easily
 his_df = pd.DataFrame()
@@ -46,12 +42,12 @@ with open(history_data_file, 'wb') as his_file: # override history_data
 
 """predict"""
 # load pickle file of processed weather data
-process_data_input = os.path.join(cur_path, 'prediction_data/processed_data.pkl')
+process_data_input = 'machine_learning_for_weather_forecast/prediction_data/processed_data.pkl'
 with open(process_data_input, 'rb') as pickle_f_in:
     df = pickle.load(pickle_f_in)
 
 # load pickle file of weather data for training
-train_data_input = os.path.join(cur_path, 'data/processed_data.pkl')
+train_data_input = 'machine_learning_for_weather_forecast/data/processed_data.pkl'
 with open(train_data_input, 'rb') as train_pickle_f_in:
     train_df = pickle.load(train_pickle_f_in)
 
@@ -71,9 +67,9 @@ learning_rate_icon = 0.02
 lamda_icon = 0.08
 scaling_factor_icon = 1
 # read icon theta and bias
-with open(os.path.join(cur_path, "prediction_data/icon_theta.pkl"), 'rb') as icon_theta_f:
+with open("machine_learning_for_weather_forecast/prediction_data/icon_theta.pkl", 'rb') as icon_theta_f:
     icon_theta = pickle.load(icon_theta_f)
-with open(os.path.join(cur_path, "prediction_data/icon_bias.pkl"), 'rb') as icon_bias_f:
+with open("machine_learning_for_weather_forecast/prediction_data/icon_bias.pkl", 'rb') as icon_bias_f:
     icon_bias = pickle.load(icon_bias_f)
 # initialize icon regressor
 regressor_icon = NN_regressor(X_icon_train, hidden_units_icon, output_num_icon, learning_rate_icon, lamda_icon, scaling_factor_icon, "classification")
@@ -92,9 +88,9 @@ learning_rate_tempMax = 0.002
 lamda_tempMax = 10
 scaling_factor_tempMax = 1
 # read tempMax theta and bias
-with open(os.path.join(cur_path, "prediction_data/max_temperature_theta.pkl"), 'rb') as tempMax_theta_f:
+with open("machine_learning_for_weather_forecast/prediction_data/max_temperature_theta.pkl", 'rb') as tempMax_theta_f:
     tempMax_theta = pickle.load(tempMax_theta_f)
-with open(os.path.join(cur_path, "prediction_data/max_temperature_bias.pkl"), 'rb') as tempMax_bias_f:
+with open("machine_learning_for_weather_forecast/prediction_data/max_temperature_bias.pkl", 'rb') as tempMax_bias_f:
     tempMax_bias = pickle.load(tempMax_bias_f)
 # initialize max temperature regressor
 regressor_tempMax = NN_regressor(X_tempMax_train, hidden_units_tempMax, output_num_tempMax, learning_rate_tempMax, lamda_tempMax, scaling_factor_tempMax, "regression")
@@ -108,9 +104,9 @@ learning_rate_tempMin = 0.004
 lamda_tempMin = 13
 scaling_factor_tempMin = 1
 # read tempMin theta and bias
-with open(os.path.join(cur_path, "prediction_data/min_temperature_theta.pkl"), 'rb') as tempMin_theta_f:
+with open("machine_learning_for_weather_forecast/prediction_data/min_temperature_theta.pkl", 'rb') as tempMin_theta_f:
     tempMin_theta = pickle.load(tempMin_theta_f)
-with open(os.path.join(cur_path, "prediction_data/min_temperature_bias.pkl"), 'rb') as tempMin_bias_f:
+with open("machine_learning_for_weather_forecast/prediction_data/min_temperature_bias.pkl", 'rb') as tempMin_bias_f:
     tempMin_bias = pickle.load(tempMin_bias_f)
 # initialize min temperature regressor
 regressor_tempMin = NN_regressor(X_tempMin_train, hidden_units_tempMin, output_num_tempMin, learning_rate_tempMin, lamda_tempMin, scaling_factor_tempMin, "regression")
@@ -124,7 +120,7 @@ today = datetime.today()
 tomorrow = today + timedelta(days=1)
 weekday = tomorrow.strftime('%A')
 
-with open(os.path.join(cur_path, "new_forecast.json"), 'w') as json_f:
+with open("machine_learning_for_weather_forecast/new_forecast.json", 'w') as json_f:
     json_f.write("{\n")
     json_f.write(f'\t"what_day" : "{weekday}",\n')
     json_f.write(f'\t"forecast_date" : "{tomorrow.month}/{tomorrow.day}",\n')
