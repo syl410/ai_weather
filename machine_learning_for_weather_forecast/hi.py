2ea027590d4cba2abf75ec9fa0ae19af66133516
Ubuntu <ubuntu@ip-172-31-23-137.us-east-2.compute.internal>
2021-01-02 06:12:20 +0000

    test

---
+++ machine_learning_for_weather_forecast/hi.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import pandas as pd
+#import numpy as np
+#from sklearn.metrics import explained_variance_score, mean_absolute_error, median_absolute_error
+#from sklearn.model_selection import train_test_split
+#import pickle
+#import statistics
+#import os
+#import random
+#import os.path
+#import time
+#from datetime import datetime, timedelta
+#from neural_network_model import *
+#from weather_module import *
+#from constant import *
+print("hi")
